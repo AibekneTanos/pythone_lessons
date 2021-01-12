13e2e3374f227019f9f3b554bfac8d2338123d9b
AibekneTanos <76169724+AibekneTanos@users.noreply.github.com>
2021-01-12 18:56:47 +0600

    Add files via upload

--- lessonnumbersix.py
+++ lessonnumbersix.py
@@ -6,12 +6,12 @@ for i in range(9):
     krestik = int(input("Введите номер столбика что бы поставить крестик "))
     krestikk = int(input("Введите номер ряда "))
     pole[krestik][krestikk] = 'X'
-    print(pole)
+    print(f'{pole[0]} \n {pole[1]} \n {pole[2]}')
 
 
     nolik = int(input("Введите номер столбика что бы поставить нолик "))
     nolikk = int(input("Введите номер ряда"))
     pole[nolik][nolikk] = 'O'
-    print(pole)
+    print(f'{ pole[0] } \n {pole[1]} \n {pole[2]}')
 
 
